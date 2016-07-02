84f86af3ee94422df5b47c05c7f1045451062ca3
Shirish Cherivirala <shirish.chervirala@gmail.com>
2016-07-02 14:45:28 -0500

    Task-4 adds recent scope to album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,10 @@
 class Album < ActiveRecord::Base
   belongs_to :publisher
   has_many :songs
-  
+
   validates :name, presence: true, length: { in: 2..50 }
   validates :cover_art, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
+
+  scope :recent, -> (n) { where('released_on >= ?', 4.months.ago).order(released_on: :desc).limit(n) }
 end
